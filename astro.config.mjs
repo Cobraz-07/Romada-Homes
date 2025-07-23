af2f549109dec55857be9a08acdb62498aa72f65
Rafael Pérez de Lema <69080754+Cobraz-07@users.noreply.github.com>
2025-07-23 20:51:00 +0200

    end for the day
    The landing is halfway done, will convert the cards into a list

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite'
+ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [icon()],
 	vite: {
     plugins: [tailwindcss()],
   },
